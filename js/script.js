4f4f76c632cad228d851c19bd6e74c61c399a9fc
Alinadf <110921794+Alina5475847231@users.noreply.github.com>
2022-10-16 14:52:01 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    burger = $('.header__burger');
-    menu = $('.header__nav');
-
-    burger.on("click", function() {
-        menu.toggleClass('show');
-        $(this).toggleClass('active');
-    });
-});
