8ff1235c067ecf3b8b473830936772230cee5fc7
Alinadf <110921794+Alina5475847231@users.noreply.github.com>
2022-10-29 20:54:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.projects__inner').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    centerMode: true,
+    variableWidth: true,
+    });
+});
