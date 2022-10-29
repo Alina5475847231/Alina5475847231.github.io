36d46097c486792b1fb99dedb4c12fc6ebf7d2be
Alinadf <110921794+Alina5475847231@users.noreply.github.com>
2022-10-29 22:27:07 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.projects__inner').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    centerMode: true,
-    variableWidth: true,
-    });
-});
