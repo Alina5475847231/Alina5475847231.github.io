ead1b5867bc6b0efbf8aab29eb42540e43d63a3a
Alinadf <110921794+Alina5475847231@users.noreply.github.com>
2022-10-31 22:01:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,226 @@
+$(document).ready(function() {
+    /* Слайдер */
+    $('.projects__inner').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    centerMode: true,
+    variableWidth: true,
+    });
+
+    
+
+    $('.mob__slide').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+    });
+
+    $('.team__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
+
+    /* Попап */
+
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+    modalCall.on("click", function() {
+        let $this = $(this);
+        let modalId = $(this).data('modal');
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalId).find(".modal__dialog").css({
+                opacity: "1"
+            });
+        }, 200);
+    });
+
+    modalClose.on("click", function() {
+        let $this = $(this);
+        let modalParent = $(this).parents('.modal');
+
+        modalParent.find(".modal__dialog").css({
+            opacity: "0"
+        });
+
+        setTimeout(function() {
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $('.modal').on("click", function() {
+        let $this = $(this);
+        $this.find(".modal__dialog").css({
+            opacity: "0"
+        });
+
+        setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $('.modal__dialog').on("click", function(event) {
+        event.stopPropagation();
+    });
+
+    /* Бриф */
+
+    const formCall = $("[data-form]");
+    const formClose = $("[data-close]");
+
+    formCall.on("click", function() {
+        let $this = $(this);
+        let formId = $(this).data('form');
+
+        $(formId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        console.log(formId);
+    });
+
+    formClose.on("click", function() {
+        let $this = $(this);
+        let formParent = $this.parents('.form');
+
+        formParent.removeClass('show');
+        $("body").removeClass('no-scroll');
+    });
+
+    /* Радиокнопки */
+
+    $.each($('.form__radiobutton'), function(index, val) {
+        if($(this).find('input').prop('checked')==true) {
+            $(this).addClass('active');
+        }
+    });
+
+    $(document).on("click", '.form__radiobutton', function(event) {
+        $(this).parents('.form__radiobuttons').find('.form__radiobutton').removeClass('active');
+        $(this).parents('.form__radiobuttons').find('.form__radiobutton input').prop('checked', false);
+        $(this).toggleClass('active');
+        $(this).find('input').prop('checked', true);
+        return false;
+    });
+
+    /* Бургер */
+
+    burger = $('.header__burger');
+    body = $('.header__mobile');
+
+    burger.on("click", function() {
+        body.toggleClass('active');
+        $(this).toggleClass('active');
+        $("body").toggleClass('no-scroll');
+    });
+
+    /* Фиксированная шапка */
+
+    let header = $('#header');
+    let intro = $('#intro');
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll load resize", function() {
+
+        scrollPos = $(this).scrollTop();
+
+        if(scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+        burger.removeClass('active');
+        body.removeClass('active');
+        $("body").removeClass('no-scroll');
+    });
+
+    /* Скролл */
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        console.log(elementOffset);
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 100
+        }, 1000);
+    });
+});
+
+/* Анимация */
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if(animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if(animItem.classList.contains('._anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
+
+function textAreaAdjust(element) {
+    element.style.height = "1px";
+    element.style.height = (5+element.scrollHeight)+"px";
+}
+
+let slideIndex = 1;
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
+currentSlide(n);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide(){
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide(){
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    // n = showSlides(slideIndex);
+}
