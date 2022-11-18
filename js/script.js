c27eaa119a29ca707ea8862f3dbe62d98c173b53
Alinadf <110921794+Alina5475847231@users.noreply.github.com>
2022-11-18 19:29:31 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  /* Выезжающий сайдбар */
-
-  $('.burger__sidebar').on("click", function() {
-    $(this).toggleClass('active');
-    $('.sidebar').toggleClass('show');
-  });
-});
