d45b31f20700a060dcb1f6c8404d68b0c7a33e04
Alinadf <110921794+Alina5475847231@users.noreply.github.com>
2022-12-20 19:38:47 +0300

    Delete js directory

--- js/burger.js
+++
@@ -1,8 +0,0 @@
-burger = $('.header__burger');
-    body = $('.header__nav');
-
-    burger.on("click", function() {
-        body.toggleClass('active');
-        $(this).toggleClass('active');
-        $("body").toggleClass('no-scroll');
-    });
