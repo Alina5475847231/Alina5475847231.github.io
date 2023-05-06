6f112c578e684e4e547d4b8e7f3914312c7a1858
Alinadf <110921794+Alina5475847231@users.noreply.github.com>
2023-05-06 14:01:24 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.menu__head').click(function(event) {
-        if($('.menu').hasClass('one')) {
-            $('.menu__head').not($(this)).removeClass('active');
-            $('.menu__submenu').not($(this).next()).slideUp(300);
-        }
-        $(this).toggleClass('active').next().slideToggle(300);
-    });
-});
